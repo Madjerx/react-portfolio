767d708ec957d7096467ef1e45ed0b7ae6c0bc32
Madjerx <madjerx@hotmail.fr>
2023-04-27 14:35:18 +0200

    start home component

---
+++ src/components/header/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./home.css";
+import Social from "./Social";
+import Data from "./Data";
+
+const Home = () => {
+  return (
+    <section className="home__section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social />
+          <Data />
+
+          <div className="home__img"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/components/header/home/Social.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="/" className="home__social-icon" target="_blank">
+        <i className="uil uil-github nav__icon"></i>
+      </a>
+      <a href="/" className="home__social-icon" target="_blank">
+        <i className="uil uil-linkedin nav__icon"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
