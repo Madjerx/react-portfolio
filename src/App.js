e267d58d58d7c0659b1d938655449fb7d277ab50
Madjerx <madjerx@hotmail.fr>
2023-04-28 17:35:09 +0200

    change home folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import About from "./components/about/About";
 import Header from "./components/header/Header";
-import Home from "./components/header/home/Home";
 import Skills from "./components/skills/Skills";
+import Home from "./components/home/Home"
 
 function App() {
   return (
