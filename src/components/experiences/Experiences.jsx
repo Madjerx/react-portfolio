1b0f8b469f83d16bc69977105f391d0bec2c0869
Madjerx <madjerx@hotmail.fr>
2023-04-29 10:02:22 +0200

    end of experiences component

--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./experiences.css";
 
 const Experiences = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <section className="exp section" id="about">
       <h2 className="section__title">Expériences</h2>
@@ -9,21 +14,41 @@ const Experiences = () => {
 
       <div className="exp__container container">
         <div className="exp__tabs">
-          <div className="exp__button button--flex">
-            <i className="uil uil-graduation-cap exp__active exp__icon"></i>{" "}
-            Formations
+          <div
+            className={
+              toggleState === 1
+                ? "exp__button exp__active button--flex"
+                : "exp__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap exp__icon"></i> Formations
           </div>
 
-          <div className="exp__button button--flex">
-            <i className="uil uil-briefcase-alt exp__icon"></i> Expériences
+          <div
+            className={
+              toggleState === 2
+                ? "exp__button exp__active button--flex"
+                : "exp__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <i className="uil uil-briefcase-alt exp__icon"></i>
+            Expériences
           </div>
         </div>
 
         <div className="exp__sections">
-          <div className="exp__content exp__content exp__content-active">
+          <div
+            className={
+              toggleState === 1
+                ? "exp__content exp__content-active"
+                : "exp__content"
+            }
+          >
             <div className="exp__data">
               <div>
-                <h3 className="exp__title>">
+                <h3 className="exp__title">
                   Développement FullStack Java / Angular
                 </h3>
                 <span className="exp__subtitle">Aélion</span>
@@ -39,11 +64,30 @@ const Experiences = () => {
             </div>
 
             <div className="exp__data">
+              <div></div>
+
               <div>
-                <h3 className="exp__title>">Développement Web</h3>
+                <span className="exp__rounder"></span>
+                <span className="exp__line"></span>
+              </div>
+
+              <div>
+                <h3 className="exp__title">Développement Web / SEO</h3>
                 <span className="exp__subtitle">Autodidacte</span>
                 <div className="exp__calender">
                   <i className="uil uil-calendar-alt"></i> 2019 - 2022
+                </div>
+              </div>
+            </div>
+
+            <div className="exp__data">
+              <div>
+                <h3 className="exp__title">
+                  Gestion des Risques et Stratégie d'Entreprise
+                </h3>
+                <span className="exp__subtitle">IAE Toulouse</span>
+                <div className="exp__calender">
+                  <i className="uil uil-calendar-alt"></i> 2010 - 2015
                 </div>
               </div>
 
@@ -54,10 +98,16 @@ const Experiences = () => {
             </div>
           </div>
 
-          <div className="exp__content">
+          <div
+            className={
+              toggleState === 2
+                ? "exp__content exp__content-active"
+                : "exp__content"
+            }
+          >
             <div className="exp__data">
               <div>
-                <h3 className="exp__title>">
+                <h3 className="exp__title">
                   Développeur FullStack Java / Angular
                 </h3>
                 <span className="exp__subtitle">Inetum</span>
@@ -73,19 +123,21 @@ const Experiences = () => {
             </div>
 
             <div className="exp__data">
+              <div></div>
+
               <div>
-                <h3 className="exp__title>">
+                <span className="exp__rounder"></span>
+                <span className="exp__line"></span>
+              </div>
+
+              <div>
+                <h3 className="exp__title">
                   Développeur Web et Consultant SEO
                 </h3>
                 <span className="exp__subtitle">Freelance</span>
                 <div className="exp__calender">
                   <i className="uil uil-calendar-alt"></i> 2019 - 2022
                 </div>
-              </div>
-
-              <div>
-                <span className="exp__rounder"></span>
-                <span className="exp__line"></span>
               </div>
             </div>
           </div>
