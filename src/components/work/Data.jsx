d66faa86d51fec8054addddbc861f90fa3e8d548
Madjerx <madjerx@hotmail.fr>
2023-05-03 12:18:28 +0200

    add works and data components

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,26 @@
+export const projectsData = [
+    {
+        id:1,
+        image: Work1,
+        title: 'Web design',
+        category: 'web'
+    },
+    {
+        id:2,
+        image: Work2,
+        title: 'Web design',
+        category: 'web'
+    },
+    {
+        id:4,
+        image: Work4,
+        title: 'Web design',
+        category: 'web'
+    },
+    {
+        id:5,
+        image: Work5,
+        title: 'Web design',
+        category: 'web'
+    },
+]
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './work.css'
+import Works from './Works'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div className='work section' id='portfolio'>
+      <h2 className='section__title'>Portfolio</h2>
+      <span className='section__subtitle'>Projets les plus récents</span>
+
+      <Works />
+    </div>
   )
 }
 
